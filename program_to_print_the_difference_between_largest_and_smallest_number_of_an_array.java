e473984c0d8dfe6f62aaf089d486c44fa59db8e0
Priyanshuraj2712 <114230655+Priyanshuraj2712@users.noreply.github.com>
2023-08-09 19:33:15 +0530

    Create program_to_print_the_difference_between_largest_and_smallest_number_of_an_array.java

---
+++ program_to_print_the_difference_between_largest_and_smallest_number_of_an_array.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Diff {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of an array");
+        int n = sc.nextInt();
+        System.out.println("Enter the element of the array");
+        int arr[] = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        int max = arr[0];
+        int min = arr[0];
+        for(int i=0;i<n;i++){
+            if(arr[i]>max){
+                max = arr[i];
+            }
+            if(arr[i]<min){
+                min = arr[i];
+            }
+        }
+        int diff = max - min;
+        System.out.println("Difference is: "+diff);
+    }
+}
